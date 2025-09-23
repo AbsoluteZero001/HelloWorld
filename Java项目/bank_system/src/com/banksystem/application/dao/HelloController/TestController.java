71bfd70edb87afc1080ad8480a135d648f0803cb
TANGJINJIE <TANGJINJIE2006@gmail.com>
2025-09-24 01:05:09 +0800

    update

--- Java项目/bank_system/src/com/banksystem/application/dao/HelloController/TestController.java
+++ Java项目/bank_system/src/com/banksystem/application/dao/HelloController/TestController.java
@@ -1,22 +1,36 @@
 package com.banksystem.application.dao.HelloController;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 
+import com.alibaba.fastjson.JSONObject;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
-
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/hello_world")
+@WebServlet("/login")
 public class TestController extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        super.doGet(req, resp);
-        String name = req.getParameter("name");
-        System.out.println(name);
-        resp.setContentType("text/html/; charset=UTF-8"); //浏览器不识别text/plain，所以加上charset=UTF-8
-        resp.getWriter().println("Hello_world " + name + "!");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        BufferedReader reader = req.getReader();
+        StringBuffer sb = new StringBuffer();
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
+        }
+        System.out.println(sb.toString());
+        JSONObject jsonObject = JSONObject.parseObject(sb.toString(),JSONObject.class);
+        String mobile = jsonObject.getString("mobile");
+        String pwd = jsonObject.getString("pwd");
+        JSONObject json = new JSONObject();
+        json.put("code", 200);
+        json.put("msg", "success");
+        json.put("data", mobile);
+        resp.getWriter().write(json.toJSONString());
+
     }
 }
